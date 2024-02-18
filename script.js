af19e15fa366243ec08fdaab176de38105920a5c
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-02-18 13:04:38 +0100

    Add base game logic (win/lose/tie conditions)!

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const choices = ["rock", "paper", "scissors"];
 
-let playerChoice = prompt('Rock, paper, or scissors? ').toLowerCase();
+// Player choice
+const playerChoice = prompt('Rock, paper, or scissors? ').toLowerCase();
 
 // Computer choice
-let getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)];
+const getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)].toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+    console.log('Player 😃: ' + playerSelection);
+    console.log('Computer 🤖: ' + computerSelection);
+
+    // Win conditions
+    if (
+        playerSelection === 'rock' && computerSelection === 'scissors' ||
+        playerSelection === 'scissors' && computerSelection === 'paper' ||
+        playerSelection === 'paper' && computerSelection === 'rock'
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }
+
+    // Lose conditions
+    if (
+        computerSelection === 'rock' && playerSelection === 'scissors' ||
+        computerSelection === 'scissors' && playerSelection === 'paper' ||
+        computerSelection === 'paper' && playerSelection  === 'rock'
+    ) {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    }
+    
+    // Tie condition
+    if (playerSelection === computerSelection) {
+        return `It's a tie! You both chose ${playerSelection}!`;
+    }
+}
+console.log(playRound(playerChoice, getComputerChoice()));
