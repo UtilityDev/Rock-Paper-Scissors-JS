a1c26a9d7d280b6b7987578b554c59e531183574
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-02-18 13:25:33 +0100

    Add playGame() function to play an an entire game and calculate a winner!

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const choices = ["rock", "paper", "scissors"];
 
-// Player choice
-const playerChoice = prompt('Rock, paper, or scissors? ').toLowerCase();
-
 // Computer choice
 const getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)].toLowerCase();
+
+let playerScore = 0;
+let computerScore = 0;
+
+// Get score
+function printScore() {
+    console.log('Player Score: ' + playerScore);
+    console.log('Computer Score: ' + computerScore);
+}
 
 function playRound(playerSelection, computerSelection) {
     console.log('Player 😃: ' + playerSelection);
@@ -16,6 +22,7 @@ function playRound(playerSelection, computerSelection) {
         playerSelection === 'scissors' && computerSelection === 'paper' ||
         playerSelection === 'paper' && computerSelection === 'rock'
     ) {
+        playerScore++;
         return `You win! ${playerSelection} beats ${computerSelection}!`;
     }
 
@@ -25,6 +32,7 @@ function playRound(playerSelection, computerSelection) {
         computerSelection === 'scissors' && playerSelection === 'paper' ||
         computerSelection === 'paper' && playerSelection  === 'rock'
     ) {
+        computerScore++;
         return `You lose! ${computerSelection} beats ${playerSelection}!`;
     }
     
@@ -33,4 +41,23 @@ function playRound(playerSelection, computerSelection) {
         return `It's a tie! You both chose ${playerSelection}!`;
     }
 }
-console.log(playRound(playerChoice, getComputerChoice()));
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        // Player choice
+        const playerChoice = prompt('Rock, paper, or scissors? ').toLowerCase();
+        console.log(playRound(playerChoice, getComputerChoice()));
+
+        printScore();
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`You win! You beat the computer by ${playerScore - computerScore} point(s)!`);
+    } else if (computerScore > playerScore) {
+        console.log(`You lost! The computer beat you by ${computerScore - playerScore} point(s)!`);
+    } else if (playerScore === computerScore) {
+        console.log(`It's a tie! Both you and the computer got ${playerScore} point(s)!`);
+    }
+}
+
+playGame();
